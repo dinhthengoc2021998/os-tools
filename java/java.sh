be8544de4c570aa1823cfbadc5550036eee539bb
ngocdt <dinhthengoc2021998@gmail.com>
2023-06-17 08:52:10 +0700

    Add java:11.0.0.1

--- java/java.sh
+++ java/java.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 install_dir=/opt/os-tools/java
-jdk_version=20
+jdk_version=11
 
-wget https://download.java.net/openjdk/jdk$jdk_version/ri/openjdk-$jdk_version+36_linux-x64_bin.tar.gz -O $install_dir/openjdk-$jdk_version+36_linux-x64_bin.tar.gz
-tar -xzvf $install_dir/openjdk-$jdk_version+36_linux-x64_bin.tar.gz -C $install_dir
-rm -rf $install_dir/openjdk-$jdk_version+36_linux-x64_bin.tar.gz
+wget https://download.java.net/openjdk/jdk$jdk_version.0.0.1/ri/openjdk-$jdk_version.0.0.1_linux-x64_bin.tar.gz \
+    -O $install_dir/openjdk-$jdk_version.0.0.1_linux-x64_bin.tar.gz
+tar -xzvf $install_dir/openjdk-$jdk_version.0.0.1_linux-x64_bin.tar.gz -C $install_dir
+rm -rf $install_dir/openjdk-$jdk_version.0.0.1_linux-x64_bin.tar.gz
 rm -rf /var/lib/jdk-$jdk_version
-mv jdk-$jdk_version/ /var/lib/jdk-$jdk_version
+mv jdk-$jdk_version.0.0.1/ /var/lib/jdk-$jdk_version
 ln -sf /var/lib/jdk-$jdk_version/bin/java /usr/bin/java
 ln -sf /var/lib/jdk-$jdk_version/bin/javac /usr/bin/javac
+ln -sf /var/lib/jdk-$jdk_version/bin/jar /usr/bin/jar

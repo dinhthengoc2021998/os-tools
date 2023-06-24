e88a73f6c1b770cdebb6240fc5adb72d94ef4f7d
ngocdt <dinhthengoc2021998@gmail.com>
2023-06-25 00:38:14 +0700

    Update Install NeoVim

--- neovim/neovim.sh
+++ neovim/neovim.sh
@@ -10,23 +10,11 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 # Extract appimage && expose nvim globally
 ./nvim.appimage --appimage-extract
+rm -rf $install_dir/nvim.appimage
 ./squashfs-root/AppRun --version
-# Optional: exposing nvim globally.
-sudo ln -sf $install_dir/squashfs-root/AppRun /usr/bin/nvim
+rm -rf /var/lib/nvim
+mv $install_dir/squashfs-root /var/lib/nvim 
 
-## Install for Python Development
-# Linux / Macos (unix)
-rm -rf ~/.config/nvim
-rm -rf ~/.local/share/nvim
-# # Install NvChad
-# git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
-#
-# ## Install Package for Python Development
-# apt update
-# apt upgrade
-# apt install -y python3.10-venv
-#
-# # Use this Commands to Install User-Configs for Python
-# # After Run the first time of VIM.
-# #mkdir -p $nvim_dir/lua/custom
-# #cp -rf neovim-user-configs/* $nvim_dir/lua/custom/
+# Optional: exposing nvim globally.
+sudo ln -sf /var/lib/nvim/AppRun /usr/bin/nvim
+

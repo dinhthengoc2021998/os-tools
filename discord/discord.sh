66125503124c5e9a92c1134a5e45ea74fea22e78
ngocdt <ngodct@vivas.vn>
2023-07-27 15:16:40 +0700

    Create a new branch for install tools for Centos7 OS

--- discord/discord.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-install_dir=/opt/os-tools/discord
-wget "https://discord.com/api/download?platform=linux&format=deb" -O $install_dir/discord.deb
-sudo apt install -y $install_dir/discord.deb
-rm -rf $install_dir/discord.deb
--- vscode-extensions/install-from-list.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-extensions_file=/opt/.vscode/extensions.list
-cat /opt/.vscode/extensions.list | xargs -L 1 code --install-extension

812e99b649c7c86d73f608696da5bcd017a4c6f6
ngocdt <dinhthengoc2021998>
2023-08-01 09:06:30 +0700

    Add config for htop

--- utils/yum-utils.sh
+++ utils/yum-utils.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+################
+# --- Config ---
+################
+install_dir=/opt/os-tools/utils
 
 #################
 # --- Network ---
 #################
 sudo yum install -y epel-release
 sudo yum install -y telnet
+# --- htop ---
 sudo yum install -y htop
+touch ~/.config/htop/htoprc
+cat $install_dir/htoprc >> ~/.config/htop/htoprc
 

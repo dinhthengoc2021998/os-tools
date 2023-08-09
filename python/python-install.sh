a08f70d68d1fcb6dfed8b73c093e35372fa65299
ngocdt <dinhthengoc2021998>
2023-08-09 15:35:19 +0700

    Add script to install python3.10

---
+++ python/python-install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+###########################
+# --- Working Directory ---
+###########################
+path_scripts=$(readlink -f "$0")
+dir_scripts=$(dirname $path_scripts)
+echo "#########################################"
+echo "Path of Scripts:      => $path_scripts"
+echo "Directory of Scripts: => $dir_scripts"
+echo "#########################################"
+
+######################
+# --- Download ---
+######################
+yum install gcc openssl-devel bzip2-devel libffi-devel -y
+curl https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz --output $dir_scripts/Python-3.10.0.tgz
+tar -xzf $dir_scripts/Python-3.10.0.tgz -C $dir_scripts
+
+######################
+# --- Installation ---
+######################
+cd $dir_scripts/Python-3.10.0
+sh configure --enable-optimizations
+make altinstall
+rm -rf $dir_scripts/Python-3.10.0.tgz $dir_scripts/Python-3.10.0

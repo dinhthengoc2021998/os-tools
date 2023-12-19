cdd2b44a283155f907825f2703de84d7f56b451b
Dinh The Ngoc <dinhthengoc2021998@gmail.com>
2023-12-19 17:31:52 +0000

    feat: instal kubectl, kubelet, kubeadm

--- k8s/k8s-install.sh
+++ k8s/k8s-install.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt -y update
+sudo apt -y install kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+kubectl version --client && kubeadm version

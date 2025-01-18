16d2cfabc6408c1562b87d15622b0a8354b33dff
Dinh Ngoc <dao.ngoc@icttm.net>
2025-01-18 17:54:20 +0700

    feat: Add neovim version and install neovim basic

--- neovim/neovim.sh
+++ neovim/neovim.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
-install_dir=/opt/os-tools/neovim
-nvim_dir=~/.config/nvim
+# Get Working Dir
+curr_dir=$(pwd)
+abs_filepath=$(readlink -f $0)
+abs_dirpath=$(dirname $abs_filepath)
+install_dir=$abs_dirpath
+nvim_dir=$HOME/.config/nvim
+echo "Installing Directory: $install_dir"
 cd $install_dir
 
-## Install NeoVim >= v9.0.1
-# Download appimage
-nvim_version="v0.10.0"
-curl -LO https://github.com/neovim/neovim/releases/download/$nvim_version/nvim.appimage
-chmod u+x nvim.appimage
-# Extract appimage && expose nvim globally
-./nvim.appimage --appimage-extract
-rm -rf $install_dir/nvim.appimage
-./squashfs-root/AppRun --version
-rm -rf /var/lib/nvim
-mv $install_dir/squashfs-root /var/lib/nvim 
+install_neovim() {
+	
+	## Install NeoVim >= v9.0.1
+	# Download appimage
+	nvim_version="v0.10.0"
+	curl -LO https://github.com/neovim/neovim/releases/download/$nvim_version/nvim.appimage
+	chmod u+x nvim.appimage
+	# Extract appimage && expose nvim globally
+	./nvim.appimage --appimage-extract
+	rm -rf $install_dir/nvim.appimage
+	./squashfs-root/AppRun --version
+	sudo rm -rf /var/lib/nvim
+	sudo mv $install_dir/squashfs-root /var/lib/nvim 
+	
+	# Optional: exposing nvim globally.
+	sudo ln -sf /var/lib/nvim/AppRun /usr/bin/nvim
+}
+check_neovim() {
+  echo "Checking neovim version"
+  nvim --version 
+}
+main() {
+  install_neovim
+  check_neovim
+}
 
-# Optional: exposing nvim globally.
-sudo ln -sf /var/lib/nvim/AppRun /usr/bin/nvim
+main
 

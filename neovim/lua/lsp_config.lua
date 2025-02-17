6366d879463e9075aeae1bd48d2d0af87867e045
Dinh Ngoc <dao.ngoc@icttm.net>
2025-02-17 14:03:38 +0700

    feat: Add lsp config

--- neovim/lua/lsp_config.lua
+++ neovim/lua/lsp_config.lua
@@ -1,13 +1,17 @@
 local lspconfig = require'lspconfig'
 
+-- Lua LSP (for Neovim)
+-- lspconfig.lua_ls.setup{}
+
 -- JavaScript/TypeScript
 lspconfig.ts_ls.setup{}
 
 -- Python
 lspconfig.pyright.setup{}
-
--- Lua LSP (for Neovim)
-lspconfig.lua_ls.setup{}
+lspconfig.pylyzer.setup{}
+lspconfig.pyre.setup{}
+lspconfig.pylsp.setup{}
 
 -- C/C++ LSP
 lspconfig.clangd.setup{}
+

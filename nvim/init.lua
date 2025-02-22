60e3b85e8568d1b213091e128a46e1f6be55f6e8
Dinh Ngoc <dao.ngoc@icttm.net>
2025-02-22 11:58:30 +0700

    feat: Add nvim install with luscripts; Add tmux use tmux-surrect

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
--- Bootstrap lazy.nvim
+local vim = vim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -15,58 +15,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  -- Reload automatic
-  { "famiu/nvim-reload" },
-
-   -- Color Theme
-  { "morhetz/gruvbox" },
-
-  -- File Browser
-  { "preservim/nerdtree" },
-  { "xuyuanp/nerdtree-git-plugin" },
-  { "ryanoasis/vim-devicons" },
-  { "tiagofumo/vim-nerdtree-syntax-highlight" },
-
-  -- File Search
-  { "junegunn/fzf", build = "fzf#install()" },
-  { "junegunn/fzf.vim" },
-
-  -- Status Bar
-  { "vim-airline/vim-airline" },
-  { "vim-airline/vim-airline-themes" },
-
-  -- Terminal Float
-  { "voldikss/vim-floaterm" },
-
-  -- Code Intelligence
-  { "neoclide/coc.nvim", branch = "release" },
-  { "jiangmiao/auto-pairs" },
-  { "alvan/vim-closetag" },
-  { "mattn/emmet-vim" },
-
-  -- Code Syntax Highlight
-  -- { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-
-  -- Debugging
-  { "puremourning/vimspector" },
-
-  -- Git
-  { "tpope/vim-fugitive" },
-
-  -- Defaults
-  { "tpope/vim-sensible" },
-  { "itchyny/lightline.vim" },
-})
-
-require("core.options")
-require("core.keymaps")
-
--- Load specific plugin configurations
-vim.cmd("source $HOME/.config/nvim/settings/nerdtree.vim")
-vim.cmd("source $HOME/.config/nvim/settings/fzf.vim")
-vim.cmd("source $HOME/.config/nvim/settings/vim-airline.vim")
-vim.cmd("source $HOME/.config/nvim/settings/vim-floaterm.vim")
-vim.cmd("source $HOME/.config/nvim/settings/key_bindings.vim")
-vim.cmd("source $HOME/.config/nvim/settings/coc.lua")
-
+-- install plugins and options
+require("vim-options")
+require("vim-helpers")
+--require("float-term")
+require("lazy").setup("plugins")
+require("colorscheme")

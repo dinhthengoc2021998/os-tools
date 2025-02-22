a1eadb62669b0ef6006d400d99a88f04c8c4ea9d
Ngoc.D.T <dinhthengoc2021998@gmail.com>
2025-02-22 23:04:09 +0700

    feat: Add tmux with more libs linux in utils

--- tmux/tmux.sh
+++ tmux/tmux.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 # Get Working Dir
-abs_filepath=$(readlink -f $0)
-abs_dirpath=$(dirname $abs_filepath)
+abs_filepath=$(readlink -f "$0")
+abs_dirpath=$(dirname "$abs_filepath")
 install_dir=$abs_dirpath
-tmux_dir="$HOME/.config/tmux"
+git_tmp_dir="$HOME/tmux"
+tmux_conf_path="$HOME/.tmux.conf"
 
 # Install tmux default
 sudo apt-get install -y tmux
-echo "$(tmux -V)"
+
+# Clean Tmux
+rm -rf "$git_tmp_dir"
+rm -rf "$tmux_conf_path"
 
 # Install Tmux plugins
-#git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-mkdir -p "$tmux_dir"
-cp -f "$install_dir/tmux.conf" "$tmux_dir/tmux.conf"
+git clone https://github.com/tmux-plugins/tpm "$HOME/tmux/plugins/tpm"
+cp -f "$install_dir/tmux.conf" "$tmux_conf_path"
+
+# Run this to source config file. 
+# Then Press `prefix + I` to install 
+# tmux source "$tmux_dir/tmux.conf"
--- utils/utils.sh
+++ utils/utils.sh
@@ -3,8 +3,12 @@
 sudo apt update -y 
 
 # NETWORKS
-sudo apt install -y traceroute iperf3 nmap iputils-ping telnet net-tools
+sudo apt install -y traceroute iperf3 nmap iputils-ping telnet \
+  net-tools iftop iproute2 tshark tcpdump dnsutils
+# This is a software with UI
+# sudo apt install -y wireshark
 
 # Basic Utils
-sudo apt install -y tree ripgrep fzf
+sudo apt install -y tree ripgrep fzf python3-venv python3-dev \
+  curl wget htop iotop iftop
 
